ea5c8ea668c3bc18f33696f4de0de87ee239698c
muhammadyusufyakubjanov8-stack <muhammadyusuf8@gmail.con>
2026-04-06 10:37:08 +0500

    for loop

---
+++ lesson6/script.js
@@ -0,0 +1,28 @@
+// For Loop
+
+// start = 1
+// start = 10
+
+// for (let raqam = 1; raqam <=10; raqam++) {
+//     console.log(raqam)
+// }
+
+// for (let raqam = 10; raqam >=1; raqam -= 2){
+//     console.log(raqam)
+// }
+
+// ================================
+
+// for (let raqam = 1; raqam <=10; raqam++){
+//     if (raqam % 2 === 1){
+//         console.log(raqam)
+//     }
+// }
+
+// ==========================================
+
+// for (let raqam = 1; raqam <=10; raqam++){
+//     if (raqam % 2 === 0){
+//         console.log(raqam)
+//     }
+// }
