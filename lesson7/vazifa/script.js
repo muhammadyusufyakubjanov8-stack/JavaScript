8eb32c25717097d294076badbb259c8cde3c30f4
muhammadyusufyakubjanov8-stack <muhammadyusuf8@gmail.con>
2026-04-13 10:40:20 +0500

    while, va do whilani qaytarishi

---
+++ lesson7/vazifa/script.js
@@ -0,0 +1,107 @@
+// 5 dan 15 gacha bo‘lgan barcha sonlarni ketma-ket console ga chiqaring.
+// let i = 5;
+// while (i <= 15) {
+//   console.log(i);
+//   i++;
+// }
+
+//  1 dan 200 gacha bo‘lgan barcha sonlarni qo‘shib, umumiy yig‘indini console ga chiqaring.
+// let yigindi = 5;
+// while (yigindi <= 15) {
+//   console.log(yigindi);
+//   yigindi++;
+// }
+
+// 1 dan 100 gacha bo‘lgan barcha juft sonlarni console ga chiqaring.
+// let i = 1 
+// while (i <= 50) {
+//   console.log(i)
+//   i += 2;
+// }
+
+// Foydalanuvchi prompt orqali N sonini kiritadi. 1 dan N gacha bo‘lgan har bir sonning kubini (son * son * son) hisoblab console ga chiqaring.
+// let n = 1
+// while (n <= 10) {
+//   console.log(n * n * n)
+//   n ++
+// }
+
+// Foydalanuvchi prompt orqali N sonini kiritadi. 1 dan N gacha bo‘lgan sonlar ichidan faqat 4 ga bo‘linadiganlarini console ga chiqaring.
+// let n = 1
+// while (n <= 3) {
+//   if (n % 4 === 0) 
+//   console.log(n)
+//   n ++
+// }
+
+// 1 dan 150 gacha bo‘lgan sonlar ichidan 3 ga ham, 5 ga ham bo‘linadigan sonlarni console ga chiqaring.
+// let i = 1
+// while (i <= 50) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i)
+//   }
+//   i ++
+// }
+
+// 20 dan 1 gacha bo‘lgan sonlarni kamayish tartibida (teskari) console ga chiqaring.
+// let i = 20
+// while (i >= 1) {
+//   console.log(i);
+//   i --;
+// }
+
+// Foydalanuvchi prompt orqali N sonini kiritadi. 1 dan N gacha bo‘lgan faqat juft sonlarni qo‘shib, yig‘indini console ga chiqaring.
+// let n = 1
+// let yigindi = 0
+// while (n <= 10) {
+//     yigindi += n
+//     n += 2
+// }
+// console.log(yigindi)
+
+//do while da 5 dan 15 gacha bo‘lgan barcha sonlarni ketma-ket console ga chiqaring.
+// let i = 5
+// do {
+//   console.log(i)
+//   i++;
+// } while (i <= 15)
+
+// do while da 1 dan 200 gacha bo‘lgan barcha sonlarni qo‘shib, umumiy yig‘indini console ga chiqaring.
+// let yigindi = 0
+// let i = 1
+// do {
+//   yigindi += i
+//   i++
+// } while (i <= 200)
+// console.log(yigindi)
+
+// do while da 1 dan 60 gacha bo‘lgan juft sonlarni (2 ga bo‘linadigan) console ga chiqaring.
+// let i = 0
+// do { 
+//   console.log(i)
+//   i+=2
+// } while (i <= 60)
+
+// do while da Foydalanuvchi prompt orqali N sonini kiritadi. 1 dan N gacha bo‘lgan har bir sonning kubini (son * son * son) hisoblab console ga chiqaring.
+// let i = 1
+// do {
+//   console.log(i * i * i)
+//   i++
+// } while (i <= 10)
+
+// do while da Foydalanuvchi prompt orqali N sonini kiritadi. 1 dan N gacha bo‘lgan sonlar ichidan faqat 4 ga bo‘linadiganlarini console ga chiqaring.
+// let i = 1
+// do {
+//   if (i % 4 === 0) {
+//     console.log(i)
+//   }
+//   i++
+// } while (i <= 10)
+
+// do while da 1 dan 150 gacha bo‘lgan sonlar ichidan 3 ga ham, 5 ga ham bo‘linadigan sonlarni console ga chiqaring.
+// let i = 1
+// do {
+//   if (i % 3 === 0 && i % 5 === 0)
+//     console.log(i)
+//   i++
+// } while (i <= 150)
---
+++ lesson8/dars/script.js
@@ -0,0 +1,117 @@
+// let toqSon = 0
+// let juftSon = 0
+// i = 1
+// while(i <= 20){
+//     if( i % 2 === 1) toqSon += i
+//     else juftSon += i
+//     i ++
+// }
+// console.log(toqSon);
+// console.log(juftSon);
+
+// let count = 0
+// let i = 1
+// while(i <= 20){
+//     if (i % 3 === 0) count ++
+//     i++
+// }
+// console.log(count);
+
+// let count2 = 0
+// let j = 1
+// do{
+//     if (j % 3 === 0) count2 ++
+//     j++ 
+// }
+// while(j <= 20)
+// console.log(count2);
+
+// let yigindi = 0
+// for(i = 30; i >= 1; i--){
+//     if(i % 3 === 0 || i % 5 === 0)
+//     yigindi += i
+// }
+// console.log(yigindi);
+
+// let i = 30
+// let yigindi = 0
+// do{
+//      if(i % 3 === 0 || i % 5 === 0)yigindi += i
+//      i--
+// }
+// while(i >= 1)   
+// console.log(yigindi);
+ 
+// let kopaytma = 1
+// for(let i = 1; i <= 100; i++){
+//     if(i % 5 === 0 && i % 7 === 0) kopaytma*=i
+// }
+// console.log(kopaytma);
+
+// let i = 1
+// let kopaytma = 1
+// while(i <= 100){
+//     if(i % 5 === 0 && i % 7 === 0)kopaytma*=i
+//     i++
+// }
+// console.log(kopaytma);
+
+// let i = 1
+// let toqSon = 0
+// let juftSon = 0
+// while(i <= 50){
+//     if(i % 2 === 1)toqSon += i
+//     else juftSon += i
+//     i++
+// }
+// let result = juftSon - toqSon
+// if(result % 2 === 1)console.log(result, "Bu toq son");
+// console.log(result , "Bu juft son.");
+
+// let i = 1
+// let toqSon = 0
+// let juftSon = 0
+// do{
+//     if(i % 2 === 1)toqSon += i
+//     else juftSon += i
+//     i++
+// }
+// while(i <= 50)
+// let result = juftSon - toqSon
+// if(result % 2 === 1)console.log(result, "Bu toq son");
+// console.log(result , "Bu juft son.");
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+//     if(i === 7) break
+// // }
+
+// for(let i = 1; i <= 10; i++){
+//     if(i %2 === 1)continue
+//     console.log(i);
+    
+// }
+
+// let j = 1
+// while(true){
+//     if(j % 5 === 0 && j % 7 === 0){
+//         console.log(j);
+//         break
+//     }
+//  j++
+// }
+
+
+// let i = 1
+// do{
+//      if(i % 5 === 0 && i % 7 === 0){
+//         console.log(i);
+//         break
+//     }
+//      i++
+// }
+// while(true){
+// }
+
+
+for(let i = 1; i <= 100; i++){
