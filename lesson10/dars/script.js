053f3d60dd47360dafcc76249dc884883d614cea
muhammadyusufyakubjanov8-stack <muhammadyusuf8@gmail.con>
2026-04-22 09:52:21 +0500

    object

---
+++ lesson10/dars/script.js
@@ -0,0 +1,52 @@
+// let a = 1
+// let b = 2
+// let c = 3
+// function eshmat(r1,r2,r3){
+//     let summa = r1 + r2 +r3
+//     return summa
+// }
+// let sum = eshmat(a,b,c)
+// console.log(sum / 2);
+
+// ==================
+
+// let a = 1
+// let b = 2
+// let c = 3
+// const eshmat = function(r1,r2,r3){
+//     let summa = r1 + r2 +r3
+//     return summa
+// }
+// let sum = eshmat(a,b,c)
+// console.log(sum / 2);
+
+// =============
+
+// let a = 1
+// let b = 2
+// let c = 3
+// const eshmat =(r1,r2,r3) =>{
+//     let summa = r1 + r2 +r3
+//     return summa
+// }
+// let sum = eshmat(a,b,c)
+// console.log(sum / 2);
+
+// ======================
+// ======================
+// ======================
+
+// let obj = {
+//     ism: "Muhammad yusuf",
+//     Familya: "Yakubjanov",
+//     Yosh: "13 Yosh",
+//     isGoschool: true,
+//     tel: "+998 95-720-1500"
+// }
+// console.log(obj);
+
+// // 1. Static usul
+// console.log(obj.ism, obj.Familya);
+// // 2 dinamic usul
+// console.log(obj["ism"], obj["Familya"]);
+
